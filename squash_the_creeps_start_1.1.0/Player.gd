054f27ea3052d4550d4b4a39c9d03d6cf42a4a7c
Kristoffer Berg Wilhelmsen <kwi059@uit.no>
2022-08-30 01:39:09 +0200

    Experimented with camera movement

--- squash_the_creeps_start_1.1.0/Player.gd
+++ squash_the_creeps_start_1.1.0/Player.gd
@@ -22,18 +22,24 @@ func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
+	#var camera = get_node("/root/Main/CameraPivot/Camera")
+	# print(camera)
+	
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		#camera.transform.origin.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+		#camera.transform.origin.x -= 1
 	if Input.is_action_pressed("move_back"):
 	# Notice how we are working with the vector's x and z axes.
 	# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
+		#camera.transform.origin.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-	
+		#camera.transform.origin.z -= 1
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
